bdf8aff538698c3e55c61cca4dca4e080ef5a0ea
A.M.S <amanpreet.singh@student.uclouvain.be>
2016-03-15 19:16:36 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include"../include/mymalloc.h"
+#include"../include/mycalloc.h"
+#include"../include/myfree.h"
 
+void main(){
+    printf("%p", mymalloc(12));
+}
